5f1e3d3c998c3e4e5ea8f7d7e0920baf57ee8e33
Medhamza93 <medhamzalahnin@gmail.com>
2023-07-28 16:14:46 -0700

    tasck 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * print_list
+ *
+ * return the number of nods
+ */
+size_t print_list(const list_t*h)
+{
+	size_t s=0;
+	while (h)
+	{
+		if(!h->str)
+			printf("[0](null)\n");
+		else
+			printf("[%u] %s\n",h->len,h->str);
+		h=h->next;
+		s++;
+	}
+	return(s);
+}
