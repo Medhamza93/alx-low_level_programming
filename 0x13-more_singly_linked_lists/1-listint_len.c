a44c0f249ca60f8f30df3feef3dee303e961aaf3
Medhamza93 <medhamzalahnin@gmail.com>
2023-08-01 16:54:42 -0700

    hamza tasck

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
 #include"lists.h"
+
 /**
  *listint_len
  *returns the number of elements in a linked lists
@@ -7,6 +8,7 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
+
 	while (h)
 	{
 		num++;
